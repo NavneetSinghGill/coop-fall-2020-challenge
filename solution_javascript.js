bb53bf64e67592576d71fb4995dcbb62b02c0de5
Navneet Singh Gill <navneet105gill@gmail.com>
2020-07-10 14:28:07 -0400

    implement saved state of operations logic

--- solution_javascript.js
+++ solution_javascript.js
@@ -6,56 +6,90 @@ class EventSourcer {
   }
 
   add(num) {
-    this.value = this.value + num;
-    this.values = this.values.push(this.value);
-    this.counter++;
+    //Replace new value
+    if(this.counter + 1 != this.values.length) {
+      this.value = this.value + num;
+      this.counter++;
+      this.values[this.counter] = num;
+    } else {
+      //Add new value
+      this.value = this.value + num;
+      this.values = this.values.concat([num]);
+      this.counter++;
+    }
 
     return this.value;
   }
   subtract(num) {
-    this.value = this.value - num;
-    this.values = this.values.push(this.value);
-    this.counter++;
+    //Replace new value
+    if(this.counter + 1 != this.values.length) {
+      this.value = this.value + num;
+      this.counter++;
+      this.values[this.counter] = num;
+    } else {
+      //Add new value
+      this.value = this.value - num;
+      this.values = this.values.concat([num * -1]);
+      this.counter++;
+    }
 
     return this.value;
   }
   undo() {
-    if(this.counter != 0) {
+    if(this.counter > 0) {
+      this.value = this.value - this.values[this.counter];
       this.counter--;
-      this.value = this.values[counter];
     }
-
     return this.value;
   }
   redo() {
     if(this.counter + 1 < this.values.length) {
       this.counter++;
-      this.value = this.values[counter];
+      this.value = this.value + this.values[this.counter];
     }
 
     return this.value;
   }
   bulk_undo(num) {
+    //If all undo's requested can be done
     if(this.counter + 1 >= num) {
-      this.counter = this.counter - num + 1;
-      this.value = this.values[this.counter];
+      for(let i = 0; i < num; i++) {
+        this.value = this.value - this.values[this.counter];
+        this.counter = this.counter--;
+      }
     } else {
+      //If not all requested undo's are possible
+      for(let i = this.counter-1; i >= 0; i--) {
+        this.value = this.value - this.values[this.counter];
+      }
       this.counter = 0;
-      this.value = this.values[this.counter];
     }
 
     return this.value;
   }
   bulk_redo(num) {
+    //If all redos can be served
     if(this.counter + num <= this.values.length) {
-      this.counter = this.counter + num - 1;
-      this.value = this.values[this.counter];
+      for(let i = 0; i < num; i++) {
+        this.counter = this.counter++;
+        this.value = this.value + this.values[this.counter];        
+      }
     } else {
-      this.counter = this.values.length - 1;
-      this.value = this.values[this.counter];
+      for(let i = this.counter; i < this.values.length; i++) {
+        this.counter = this.counter++;
+        this.value = this.value + this.values[this.counter];
+      }
     }
 
     return this.value;
+  }
+
+  setCurrentValue() {
+    let newValue = 0;
+    for(let i = 0; i<=this.counter; i++) {
+      newValue+=i;
+    }
+    this.value = newValue;
   }
 }
 
