980056f512d77ce81ec85c49029908fd4333fb75
Navneet Singh Gill <navneet105gill@gmail.com>
2020-07-10 13:31:28 -0400

    Implement the functions

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,14 +1,62 @@
 class EventSourcer {
   constructor() {
     this.value = 0;
+    this.values = [0];
+    this.counter = 0;
   }
 
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+  add(num) {
+    this.value = this.value + num;
+    this.values = this.values.push(this.value);
+    this.counter++;
+
+    return this.value;
+  }
+  subtract(num) {
+    this.value = this.value - num;
+    this.values = this.values.push(this.value);
+    this.counter++;
+
+    return this.value;
+  }
+  undo() {
+    if(this.counter != 0) {
+      this.counter--;
+      this.value = this.values[counter];
+    }
+
+    return this.value;
+  }
+  redo() {
+    if(this.counter + 1 < this.values.length) {
+      this.counter++;
+      this.value = this.values[counter];
+    }
+
+    return this.value;
+  }
+  bulk_undo(num) {
+    if(this.counter + 1 >= num) {
+      this.counter = this.counter - num + 1;
+      this.value = this.values[this.counter];
+    } else {
+      this.counter = 0;
+      this.value = this.values[this.counter];
+    }
+
+    return this.value;
+  }
+  bulk_redo(num) {
+    if(this.counter + num <= this.values.length) {
+      this.counter = this.counter + num - 1;
+      this.value = this.values[this.counter];
+    } else {
+      this.counter = this.values.length - 1;
+      this.value = this.values[this.counter];
+    }
+
+    return this.value;
+  }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
